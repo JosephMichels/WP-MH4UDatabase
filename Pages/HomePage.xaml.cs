3d6af20e4162b90e399b0831603bdfcb7478d79c
Joseph Michels <j0mich01+github@gmail.com>
2015-10-27 09:09:19 -0400

    Pages 6

---
+++ Pages/HomePage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using MH4U_Database.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace MH4U_Database.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HomePage : BasePage
+    {
+
+        public HomePage():base()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void OptionSelected(object sender, ItemClickEventArgs e)
+        {
+            if (e.ClickedItem == monsters)
+            {
+                Frame.Navigate(typeof(MonsterListPage));
+            }
+            else if (e.ClickedItem == items)
+            {
+                Frame.Navigate(typeof(ItemListPage));
+            }
+            else if (e.ClickedItem == weapons)
+            {
+                Frame.Navigate(typeof(WeaponTypePage));
+            }
+            else if (e.ClickedItem == armor)
+            {
+                Frame.Navigate(typeof(ArmorListPage));
+            }
+            else if (e.ClickedItem == skills)
+            {
+                Frame.Navigate(typeof(SkillListPage));
+            }
+            else if (e.ClickedItem == quests)
+                Frame.Navigate(typeof(QuestListPage));
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (e.Parameter != null && e.Parameter is int)
+            {
+                Frame.BackStack.Clear();
+            }
+        }
+    }
+}
---
+++ Pages/ItemDetailsPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using MH4U_Database.Common;
+using MH4U_Database.Database;
+using MH4U_Database.ViewModel;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace MH4U_Database.Pages
+{
+    public sealed partial class ItemDetailsPage : BasePage
+    {
+
+        public ItemDetailsPage()
+        {
+            InitializeComponent();
+        }
+        
+
+        protected override void LoadState(object sender, LoadStateEventArgs e)
+        {
+            if (e.PageState != null)
+            {
+                if (e.PageState.ContainsKey(PageValues.SELECTED_PIVOT_INDEX))
+                    pivot.SelectedIndex = (int)e.PageState[PageValues.SELECTED_PIVOT_INDEX];
+            }
+
+        }
+
+        protected override void SaveState(object sender, SaveStateEventArgs e)
+        {
+            e.PageState.Add(PageValues.SELECTED_PIVOT_INDEX, pivot.SelectedIndex);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            int id = (int)e.Parameter;
+            DataContext = new ItemDetailsViewModel(id);
+            base.OnNavigatedTo(e);
+        }
+
+        private void ComponentClicked(object sender, ItemClickEventArgs e)
+        {
+            if (e.ClickedItem is Component)
+            {
+                //Navigate to the item details page.
+                Component c = (Component)e.ClickedItem;
+                if (c.created_item_type.Equals("Weapon"))
+                    Frame.Navigate(typeof(WeaponDetailsPage), c.created_item_id);
+                else if (c.created_item_type.Equals("Armor"))
+                    Frame.Navigate(typeof(ArmorDetailsPage), c.created_item_id);
+                else
+                    Frame.Navigate(typeof(ItemDetailsPage), c.created_item_id);
+            }
+        }
+
+        private void MonsterClicked(object sender, ItemClickEventArgs e)
+        {
+            if(e.ClickedItem != null && e.ClickedItem is HuntingReward)
+            {
+                HuntingReward hr = (HuntingReward)e.ClickedItem;
+                Frame.Navigate(typeof(MonsterDetailsPage), (int)hr.monster_id);
+            }
+        }
+    }
+}
