eed9c5ab2b8f3dfb32e2d45f95742a0673ede2f9
Joseph Michels <j0mich01+github@gmail.com>
2015-10-27 09:07:23 -0400

    Controls+Common

---
+++ Controls/SharpnessDisplay.xaml.cs
@@ -0,0 +1,51 @@
+﻿using MH4U_Database.Database;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace MH4U_Database.Controls
+{
+    public sealed partial class SharpnessDisplay : UserControl
+    {
+        public Sharpness Sharpness
+        {
+            get { return (Sharpness)GetValue(SharpnessProperty); }
+            set
+            {
+                SetValue(SharpnessProperty, value);
+            }
+        }
+
+        public int SharpnessHeight
+        {
+            get { return (int)GetValue(SharpnessHeightProperty); }
+            set
+            {
+                SetValue(SharpnessHeightProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty SharpnessProperty = DependencyProperty.Register("Sharpness",typeof(Sharpness),typeof(SharpnessDisplay), null);
+        public static readonly DependencyProperty SharpnessHeightProperty = DependencyProperty.Register("SharpnessHeight", typeof(int), typeof(SharpnessDisplay), null);
+
+        public SharpnessDisplay()
+        {
+            this.InitializeComponent();
+
+            SharpnessHeight = 4;
+        }
+    }
+}
---
+++ Controls/StretchItemsListView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MH4U_Database.Controls
+{
+    public class StrechItemsListView : ListView
+    {
+        public StrechItemsListView()
+        {
+            SizeChanged += StrechItemsListView_SizeChanged;
+        }
+
+        private void StrechItemsListView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (ItemsPanelRoot != null)
+            {
+                ItemsPanelRoot.Width = e.NewSize.Width;
+            }
+        }
+    }
+}
