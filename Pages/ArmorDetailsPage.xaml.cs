fb968f9e218034b572318037062139f404950d45
Joseph Michels <j0mich01+github@gmail.com>
2015-10-27 09:08:56 -0400

    Pages 4

---
+++ Pages/ArmorDetailsPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using MH4U_Database.Common;
+using MH4U_Database.Database;
+using MH4U_Database.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace MH4U_Database.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ArmorDetailsPage : Page
+    {
+        NavigationHelper _navHelper;
+
+        public ArmorDetailsPage()
+        {
+            this.InitializeComponent();
+
+            _navHelper = new NavigationHelper(this);
+            _navHelper.SaveState += SaveState;
+            _navHelper.LoadState += LoadState;
+        }
+
+        private void LoadState(object sender, LoadStateEventArgs e)
+        {
+            if (e.PageState != null)
+            {
+                if (e.PageState.ContainsKey(PageValues.SELECTED_PIVOT_INDEX))
+                    pivot.SelectedIndex = (int)e.PageState[PageValues.SELECTED_PIVOT_INDEX];
+            }
+
+        }
+
+        private void SaveState(object sender, SaveStateEventArgs e)
+        {
+            e.PageState.Add(PageValues.SELECTED_PIVOT_INDEX, pivot.SelectedIndex);
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            int id = (int)e.Parameter;
+            DataContext = new ArmorDetailsViewModel(id);
+            _navHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            _navHelper.OnNavigatedFrom(e);
+        }
+
+        private void ComponentSelected(object sender, ItemClickEventArgs e)
+        {
+            if(e.ClickedItem is Component)
+            {
+                //Navigate to the item details page.
+                Component c = (Component)e.ClickedItem;
+                Frame.Navigate(typeof(ItemDetailsPage), c.comp_item_id);
+            }
+        }
+    }
+}
---
+++ Pages/BasePage.cs
@@ -0,0 +1,145 @@
+﻿using MH4U_Database.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace MH4U_Database.Pages
+{
+    public class BasePage : Page
+    {
+        protected NavigationHelper navigationHelper;
+
+        protected RelayCommand mainPagesBackCommand;
+
+        public BasePage()
+        {
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.LoadState;
+            this.navigationHelper.SaveState += this.SaveState;
+
+            mainPagesBackCommand = new RelayCommand(() =>
+            {
+                if (Frame.CanGoBack)
+                    Frame.GoBack();
+                else
+                    Frame.Navigate(typeof(HomePage), 0);
+            }, () =>
+            {
+                return true;
+            });
+
+        }
+
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
+
+
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        protected virtual void LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        protected virtual void SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+
+        /// <summary>
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// <para>
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="NavigationHelper.LoadState"/>
+        /// and <see cref="NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+        /// </para>
+        /// </summary>
+        /// <param name="e">Provides data for navigation methods and event
+        /// handlers that cannot cancel the navigation request.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+        }
+
+
+        #region Handle App Bar Button Presses
+
+        protected void AppBarHomeClicked(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(HomePage), 0);
+        }
+
+        protected void AppBarMonstersClicked(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MonsterListPage));
+            Frame.BackStack.Clear();
+        }
+
+        protected void AppBarItemsClicked(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(ItemListPage));
+            Frame.BackStack.Clear();
+        }
+
+        protected void AppBarWeaponsClicked(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(WeaponTypePage));
+            Frame.BackStack.Clear();
+        }
+
+        protected void AppBarArmorClicked(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(ArmorListPage));
+            Frame.BackStack.Clear();
+        }
+
+        protected void AppBarSkillsClicked(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(SkillListPage));
+            Frame.BackStack.Clear();
+        }
+
+        protected void AppBarQuestsClicked(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(QuestListPage));
+            Frame.BackStack.Clear();
+        }
+
+        #endregion
+
+    }
+}
