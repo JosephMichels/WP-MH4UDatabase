6f7b769b97a82231a5bbb5e160fa8e850deb235c
Joedev <j0mich01+github@gmail.com>
2015-10-29 19:23:09 -0400

    Hide affinity if equal to zero

---
+++ Converters/AffinityVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace MH4U_Database.Converters
+{
+    class AffinityVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if(value != null && value is string)
+            {
+                string val = (string)value;
+                if (val.StartsWith("0")) return Visibility.Collapsed;
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
