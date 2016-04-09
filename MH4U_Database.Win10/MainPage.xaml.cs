8a0ed8848f09843b8075a912ec79f719d452d20c
Joedev <j0mich01+github@gmail.com>
2016-04-09 11:37:21 -0400

    Update project structure to support Win10

---
+++ MH4U_Database.Win10/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using App1.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataContext = new MainPageViewModel();
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            if(e.ClickedItem is NavigationEntry)
+            {
+                NavigationEntry entry = (NavigationEntry)e.ClickedItem;
+                rootFrame.Navigate(entry.Destination);
+            }
+        }
+
+        private void FrameNavigatedToPage(object sender, NavigationEventArgs e)
+        {
+            // Update the Back button depending on whether we can go Back.
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                rootFrame.CanGoBack ?
+                AppViewBackButtonVisibility.Visible :
+                AppViewBackButtonVisibility.Collapsed;
+        }
+    }
+}
---
+++ MH4U_Database.Win10/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using App1.Pages;
+using MH4U_Database.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace App1.ViewModel
+{
+
+    public class NavigationEntry
+    {
+        public string Name { get; set; }
+        public string IconPath { get; set; }
+        public Type Destination { get; set; }
+    }
+
+    public class MainPageViewModel : BaseViewModel
+    {
+
+        NavigationEntry[] _entries = new[]
+        {
+            new NavigationEntry() {Name="Monsters",IconPath="/Assets/icons_items/Book-White.png",Destination=typeof(MonsterListPage) },
+            new NavigationEntry() {Name="Items",IconPath="/Assets/icons_items/Liquid-White.png",Destination=typeof(MainPage) },
+            new NavigationEntry() {Name="Weapons",IconPath="/Assets/icons_items/charge_blade1.png",Destination=typeof(MainPage) },
+            new NavigationEntry() {Name="Armor",IconPath="/Assets/icons_items/body1.png",Destination=typeof(MainPage) },
+            new NavigationEntry() {Name="Skills",IconPath="/Assets/icons_items/Jewel-White.png",Destination=typeof(MainPage) },
+            new NavigationEntry() {Name="Quests",IconPath="/Assets/icons_items/Quest-Icon-White.png",Destination=typeof(MainPage) },
+            new NavigationEntry() {Name="Combinations",IconPath="/Assets/icons_items/Herb-White.png",Destination=typeof(MainPage) },
+            new NavigationEntry() {Name="Wyporium",IconPath="/Assets/icons_items/Scale-White.png",Destination=typeof(MainPage) },
+        };
+        public NavigationEntry[] NavigationEntries { get { return _entries; }
+            set
+            {
+                _entries = value;
+                OnPropertyChanged(nameof(NavigationEntries));
+            }
+        }
+    }
+}
